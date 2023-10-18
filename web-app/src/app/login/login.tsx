f7fe6bfce5837d50cf893901a0e2d9d8a8f6eca0
Ofri Wolfus <ofri.wolfus@gmail.com>
2023-10-18 13:37:41 +0300

    UI

--- web-app/src/app/login/login.tsx
+++ web-app/src/app/login/login.tsx
@@ -8,8 +8,11 @@ import {
 import { cn, makeStyles } from '../../../../styles/css-objects/index.ts';
 import { styleguide } from '../../../../styles/styleguide.ts';
 import { H2 } from '../../../../styles/components/typography.tsx';
+import TextField from '../../../../styles/components/inputs/TextField.tsx';
+import { Button } from '../../../../styles/components/buttons.tsx';
+import { brandLightTheme as theme } from '../../../../styles/theme.tsx';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   centeredHeader: {
     display: 'flex',
     justifyContent: 'center',
@@ -20,9 +23,44 @@ const useStyles = makeStyles((theme) => ({
   welcomeContainer: {
     marginTop: styleguide.gridbase * 4,
   },
+  contentsContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: styleguide.gridbase * 10,
+  },
   illustration: {
     marginLeft: styleguide.gridbase * 5,
-    marginTop: styleguide.gridbase * 10,
+  },
+  loginForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginRight: styleguide.gridbase * 25,
+  },
+  loginTitle: {
+    textAlign: 'center',
+    width: '100%',
+    marginBottom: styleguide.gridbase * 5,
+    marginTop: styleguide.gridbase * 5,
+  },
+  textField: {
+    width: styleguide.gridbase * 40,
+  },
+  passwordField: {
+    marginTop: styleguide.gridbase * 2,
+  },
+  loginButton: {
+    height: styleguide.gridbase * 4,
+    boxSizing: 'border-box',
+    borderRadius: 20,
+    fontSize: 14,
+    fontWeight: 500,
+    // lineHeight: 16,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: theme.primary.p9,
+    backgroundColor: theme.primary.p9,
+    marginTop: styleguide.gridbase * 6,
+    color: theme.mono.m0,
   },
 }));
 
@@ -40,7 +78,18 @@ export function LoginView({ session }: LoginViewProps) {
       <div className={cn(styles.centeredHeader, styles.welcomeContainer)}>
         <H2>Welcome to Ovvio</H2>
       </div>
-      <LoginIllustration className={cn(styles.illustration)} />
+      <div className={cn(styles.contentsContainer)}>
+        <LoginIllustration className={cn(styles.illustration)} />
+        <div className={cn(styles.loginForm)}>
+          <H2 className={cn(styles.loginTitle)}>Login</H2>
+          <TextField className={cn(styles.textField)} placeholder="Email" />
+          <TextField
+            className={cn(styles.passwordField, styles.textField)}
+            placeholder="Password"
+          />
+          <Button className={cn(styles.loginButton)}>Log In</Button>
+        </div>
+      </div>
     </div>
   );
 }
