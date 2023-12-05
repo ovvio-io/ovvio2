a59eb7630c0c995fbdef671bf6b63ee6349d96c8
Ofri Wolfus <ofri.wolfus@gmail.com>
2023-12-05 15:57:05 +0200

    Bugs--

--- base/serial-scheduler.ts
+++ base/serial-scheduler.ts
@@ -1,6 +1,11 @@
 export class SerialScheduler {
   private static readonly _namedSchedulers = new Map<string, SerialScheduler>();
-  private _lastPromise: Promise<unknown> | null = null;
+  private _pendingFunctions: (() => Promise<unknown>)[];
+  private _runningPromise: Promise<unknown> | undefined;
+
+  constructor() {
+    this._pendingFunctions = [];
+  }
 
   static get(name: string): SerialScheduler {
     let scheduler = this._namedSchedulers.get(name);
@@ -11,30 +16,42 @@ export class SerialScheduler {
     return scheduler;
   }
 
-  async run<T>(fn: () => Promise<T>): Promise<T> {
-    if (!this._lastPromise) {
-      const p = fn();
-      this._lastPromise = p;
-      try {
-        return await p;
-      } finally {
-        if (this._lastPromise === p) {
-          this._lastPromise = null;
-        }
+  private async wrapFn<T>(fn: () => Promise<T>): Promise<T> {
+    try {
+      return await fn();
+    } finally {
+      const nextFn = this._pendingFunctions.shift();
+      if (nextFn) {
+        this._runningPromise = this.wrapFn(nextFn);
       }
     }
+  }
 
-    const p = this._lastPromise.finally(async () => {
+  run<T>(fn: () => Promise<T>): Promise<T> {
+    let resolve, reject;
+    const result = new Promise<T>((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    const wrapper = async () => {
       try {
-        return await fn();
+        resolve!(await fn());
+      } catch (err: unknown) {
+        reject!(err);
       } finally {
-        if (this._lastPromise === p) {
-          this._lastPromise = null;
+        const nextFn = this._pendingFunctions.shift();
+        if (nextFn) {
+          this._runningPromise = this.wrapFn(nextFn);
+        } else {
+          this._runningPromise = undefined;
         }
       }
-    });
-
-    this._lastPromise = p;
-    return p as Promise<T>;
+    };
+    if (!this._runningPromise) {
+      this._runningPromise = wrapper();
+    } else {
+      this._pendingFunctions.push(wrapper);
+    }
+    return result;
   }
 }
