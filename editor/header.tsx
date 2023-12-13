939e6f916b95178b0b79310ba7f6f9e8965b0647
Ofri Wolfus <ofri.wolfus@gmail.com>
2023-12-13 22:55:04 +0200

    Breadcrumbs

--- editor/header.tsx
+++ editor/header.tsx
@@ -1,12 +1,15 @@
 import React, { forwardRef, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { resolveWritingDirection } from '../base/string.ts';
+import { formatTimeDiff } from '../base/date.ts';
 import { VertexManager } from '../cfds/client/graph/vertex-manager.ts';
 import { Note } from '../cfds/client/graph/vertices/note.ts';
 import { usePartialVertex } from '../web-app/src/core/cfds/react/vertex.ts';
-import { makeStyles, cn } from '../styles/css-objects/index.ts';
+import { cn, makeStyles } from '../styles/css-objects/index.ts';
 import { brandLightTheme as theme } from '../styles/theme.tsx';
 import { styleguide } from '../styles/styleguide.ts';
+import { Text, TextSm } from '../styles/components/typography.tsx';
+import { WorkspaceIndicator } from '../components/workspace-indicator.tsx';
 
 export const HEADER_HEIGHT = styleguide.gridbase * 24;
 
@@ -37,7 +40,7 @@ const useStyles = makeStyles(() => ({
     height: styleguide.gridbase * 3,
     width: styleguide.gridbase * 40,
     outline: 'none',
-    fontFamily: 'Poppins',
+    fontFamily: '\'Poppins\', \'Heebo\', sans-serif',
     fontSize: '30px',
     fontWeight: '600',
     lineHeight: styleguide.gridbase * 3,
@@ -47,6 +50,30 @@ const useStyles = makeStyles(() => ({
     whiteSpace: 'pre-wrap',
     paddingBottom: styleguide.gridbase,
     textOverflow: 'ellipsis',
+  },
+  breadCrumbsContainer: {
+    position: 'absolute',
+    top: styleguide.gridbase * 3,
+    left: styleguide.gridbase * 3.25,
+    display: 'flex',
+    alignItems: 'center',
+    color: theme.mono.m4,
+  },
+  breadCrumbsSeparator: {
+    width: '1px',
+    height: '1em',
+    backgroundColor: theme.mono.m4,
+    marginLeft: styleguide.gridbase,
+    marginRight: styleguide.gridbase,
+  },
+  breadCrumbsSlash: {
+    position: 'relative',
+    top: '2px',
+    marginRight: styleguide.gridbase,
+  },
+  breadCrumbsTitle: {
+    fontSize: '20px',
+    color: theme.mono.m10,
   },
 }));
 
@@ -62,10 +89,10 @@ export const HeaderTitle = forwardRef<HTMLInputElement, HeaderTitleProps>(
     const baseDirection = resolveWritingDirection(partialVertex.titlePlaintext);
     return (
       <input
-        key="EditorTitle"
+        key='EditorTitle'
         ref={ref}
         className={cn(styles.titleInput)}
-        type="text"
+        type='text'
         dir={baseDirection === 'rtl' ? 'rtl' : undefined}
         value={partialVertex.titlePlaintext}
         placeholder={'Name your note'}
@@ -82,8 +109,34 @@ export const HeaderTitle = forwardRef<HTMLInputElement, HeaderTitleProps>(
         autoFocus
       />
     );
-  }
+  },
 );
+
+export interface EditorBreadCrumbsProps {
+  note: VertexManager<Note>;
+}
+
+function EditorBreadCrumbs({ note }: EditorBreadCrumbsProps) {
+  const styles = useStyles();
+  const partialNote = usePartialVertex(note, [
+    'creationDate',
+    'workspace',
+    'titlePlaintext',
+  ]);
+  return (
+    <div className={cn(styles.breadCrumbsContainer)}>
+      <TextSm>{formatTimeDiff(partialNote.creationDate)}</TextSm>
+      <span className={cn(styles.breadCrumbsSeparator)}></span>
+      <WorkspaceIndicator
+        workspace={partialNote.workspace.manager}
+      />
+      <span className={cn(styles.breadCrumbsSlash)}>/</span>
+      <Text className={cn(styles.breadCrumbsTitle)}>
+        {partialNote.titlePlaintext}
+      </Text>
+    </div>
+  );
+}
 
 export interface EditorHeaderProps {
   note: VertexManager<Note>;
@@ -116,19 +169,20 @@ export function EditorHeader({ note, onFocusOnEditor }: EditorHeaderProps) {
   }, [didFocus, setDidFocus, titleInputRef]);
 
   return (
-    <div className={cn(styles.header)} key="EditorHeaderDiv">
-      <div className={cn(styles.headerMainActions)} key="EditorHeaderActions">
+    <div className={cn(styles.header)} key='EditorHeaderDiv'>
+      <EditorBreadCrumbs note={note} />
+      <div className={cn(styles.headerMainActions)} key='EditorHeaderActions'>
         <img
-          key="ExitEditorAction"
+          key='ExitEditorAction'
           className={cn(styles.headerMainActionButton)}
-          src="/icons/editor/icon/close-circle.svg"
+          src='/icons/editor/icon/close-circle.svg'
           onClick={() => {
             navigate('/');
           }}
         />
       </div>
       <HeaderTitle
-        key="EditorHeader"
+        key='EditorHeader'
         ref={titleInputRef}
         note={note}
         onEnter={onFocusOnEditor}
