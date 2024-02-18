92b7a4bc64bf37994a9427a0e3cfd6c02f22f8b8
Ofri Wolfus <ofri.wolfus@gmail.com>
2024-02-18 15:15:34 +0200

    Bug fixes

--- base/core-types/encoding/types.ts
+++ base/core-types/encoding/types.ts
@@ -18,17 +18,24 @@ export type DecodedValue =
 
 export interface Decoder<
   K extends DecodableKey = DecodableKey,
-  V extends DecodedValue = DecodedValue
+  V extends DecodedValue = DecodedValue,
 > {
   get<T extends V>(key: K, defaultValue?: T): T | typeof defaultValue;
   has(key: K): boolean;
   getDecoder(key: K, offset?: number): Decoder<K, V>;
 }
 
+export function isDecoder<
+  K extends DecodableKey = DecodableKey,
+  V extends DecodedValue = DecodedValue,
+>(v: DecodedValue): v is Decoder<K, V> {
+  return typeof (v as Decoder<K, V>).getDecoder === 'function';
+}
+
 export interface Decodable<
   K extends DecodableKey = DecodableKey,
   V extends DecodedValue = DecodedValue,
-  OT = unknown
+  OT = unknown,
 > {
   deserialize(decoder: Decoder<K, V>, options?: OT): void;
 }
