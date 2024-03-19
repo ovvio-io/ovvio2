2853d5a4566c7380416a14ed5934a86d32ced4a7
Ofri Wolfus <ofri.wolfus@gmail.com>
2024-03-19 15:15:30 +0200

    Version++

--- base/version-number.ts
+++ base/version-number.ts
@@ -22,6 +22,8 @@ export const V3_1_4: VersionNumber = tuple4Make([3, 1, 4, 0]);
 export const V3_2_0: VersionNumber = tuple4Make([3, 2, 0, 0]);
 // 17/3/2024
 export const V3_3_0: VersionNumber = tuple4Make([3, 3, 0, 0]);
+// 19/3/2024
 export const V3_4_0: VersionNumber = tuple4Make([3, 4, 0, 0]);
+export const V3_4_1: VersionNumber = tuple4Make([3, 4, 1, 0]);
 
-export const VCurrent = V3_4_0;
+export const VCurrent = V3_4_1;
