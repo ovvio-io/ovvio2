05f4667c943116accf6362a93ebec34056afb7a7
Ofri Wolfus <ofri.wolfus@gmail.com>
2024-03-19 15:14:24 +0200

    Docs++

--- server-control/launch-script.sh
+++ server-control/launch-script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Uncomment the correct URL for the environment
+# CONTROL_URL="https://ovvio2-release.s3.amazonaws.com/ovvio-control-linux-alpha.gz"
+# CONTROL_URL="https://ovvio2-release.s3.amazonaws.com/ovvio-control-linux-beta.gz"
 CONTROL_URL="https://ovvio2-release.s3.amazonaws.com/ovvio-control-linux.gz"
 
 cd /home/ec2-user
