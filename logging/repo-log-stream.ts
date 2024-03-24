59a147854b2ed8d95063af4cb8e7bac2474442a1
Ofri Wolfus <ofri.wolfus@gmail.com>
2024-03-24 13:27:56 +0200

    Shard events across 3 repositories

--- logging/repo-log-stream.ts
+++ logging/repo-log-stream.ts
@@ -5,19 +5,30 @@ import { Record } from '../cfds/base/record.ts';
 import { Scheme } from '../cfds/base/scheme.ts';
 
 export class RepoLogStream implements LogStream {
-  constructor(readonly repository: Repository<MemRepoStorage>) {}
+  constructor(readonly repositories: Repository<MemRepoStorage>[]) {}
 
   appendEntry(e: NormalizedLogEntry<LogEntry>): void {
-    if (!this.repository.hasKey(e.logId)) {
-      this.repository.setValueForKey(
-        e.logId,
-        new Record({
-          scheme: Scheme.event(),
-          data: {
-            json: JSON.stringify(e),
-          },
-        }),
-      );
+    let min = this.repositories[0];
+    if (min.hasKey(e.logId)) {
+      return;
     }
+    for (let i = 1; i < this.repositories.length; ++i) {
+      const r = this.repositories[i];
+      if (r.hasKey(e.logId)) {
+        return;
+      }
+      if (r.numberOfCommits() < min.numberOfCommits()) {
+        min = r;
+      }
+    }
+    min.setValueForKey(
+      e.logId,
+      new Record({
+        scheme: Scheme.event(),
+        data: {
+          json: JSON.stringify(e),
+        },
+      }),
+    );
   }
 }
