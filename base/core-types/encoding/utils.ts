0b4abb4d7a164208c4ec465688a7ea0d38a2892a
Ofri Wolfus <ofri.wolfus@gmail.com>
2024-04-15 13:11:33 +0300

    Types

--- base/core-types/encoding/utils.ts
+++ base/core-types/encoding/utils.ts
@@ -1,7 +1,8 @@
 import { ConstructorDecoderConfig } from './types.ts';
 
-export function isDecoderConfig<T extends ConstructorDecoderConfig<any>>(
-  v: any
+export function isDecoderConfig<T extends ConstructorDecoderConfig<unknown>>(
+  v: unknown,
 ): v is T {
-  return v !== undefined && v.hasOwnProperty('decoder');
+  // deno-lint-ignore no-prototype-builtins
+  return v !== undefined && v !== null && v.hasOwnProperty('decoder');
 }
