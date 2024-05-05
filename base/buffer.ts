eafd32ab79c4361f9f1ec342af0dc32ff1f22a69
Ofri Wolfus <ofri.wolfus@gmail.com>
2024-05-05 15:11:53 +0300

    Completely disabling buggy buffer caching

--- base/buffer.ts
+++ base/buffer.ts
@@ -1,48 +1,48 @@
 import { assert } from './error.ts';
 
-const K_BUFF_UNIT_SIZE = 32 * 1024;
+// const K_BUFF_UNIT_SIZE = 64 * 1024;
 
-const gPendingBuffers: Uint8Array[] = [];
-const gLiveBuffers = new Map<Uint8Array, Uint8Array>();
+// const gPendingBuffers: Uint8Array[] = [];
+// const gLiveBuffers = new Map<Uint8Array, Uint8Array>();
 
-for (let i = 0; i < 10; ++i) {
-  gPendingBuffers.push(new Uint8Array(K_BUFF_UNIT_SIZE));
-}
+// for (let i = 0; i < 10; ++i) {
+//   gPendingBuffers.push(new Uint8Array(K_BUFF_UNIT_SIZE));
+// }
 
 export function allocateBuffer(minBytes: number): Uint8Array {
-  let cachedBuff: Uint8Array | undefined;
-  for (let i = 0; i < gPendingBuffers.length; ++i) {
-    const buf = gPendingBuffers[i];
-    if (buf.byteLength >= minBytes) {
-      cachedBuff = gPendingBuffers.splice(i, 1)[0];
-      assert(!gPendingBuffers.includes(buf));
-      assert(!gLiveBuffers.has(buf));
-      break;
-    }
-  }
-  if (!cachedBuff) {
-    cachedBuff = new Uint8Array(
-      Math.max(1, Math.ceil(minBytes / K_BUFF_UNIT_SIZE)) * K_BUFF_UNIT_SIZE,
-    );
-  }
-  if (cachedBuff) {
-    assert(cachedBuff.byteLength >= minBytes); // Sanity check
-    cachedBuff.fill(0);
-    const res = cachedBuff.subarray(0, minBytes);
-    gLiveBuffers.set(res, cachedBuff);
-    assert(cachedBuff.byteLength >= minBytes); // Sanity check
-    return res;
-  }
+  // let cachedBuff: Uint8Array | undefined;
+  // for (let i = 0; i < gPendingBuffers.length; ++i) {
+  //   const buf = gPendingBuffers[i];
+  //   if (buf.byteLength >= minBytes) {
+  //     cachedBuff = gPendingBuffers.splice(i, 1)[0];
+  //     assert(!gPendingBuffers.includes(buf));
+  //     assert(!gLiveBuffers.has(buf));
+  //     break;
+  //   }
+  // }
+  // if (!cachedBuff) {
+  //   cachedBuff = new Uint8Array(
+  //     Math.max(1, Math.ceil(minBytes / K_BUFF_UNIT_SIZE)) * K_BUFF_UNIT_SIZE,
+  //   );
+  // }
+  // if (cachedBuff) {
+  //   assert(cachedBuff.byteLength >= minBytes); // Sanity check
+  //   cachedBuff.fill(0);
+  //   const res = cachedBuff.subarray(0, minBytes);
+  //   gLiveBuffers.set(res, cachedBuff);
+  //   assert(cachedBuff.byteLength >= minBytes); // Sanity check
+  //   return res;
+  // }
   return new Uint8Array(minBytes);
 }
 
 export function cacheBufferForReuse(buff: Uint8Array): void {
-  const origBuff = gLiveBuffers.get(buff);
-  if (origBuff) {
-    origBuff.fill(0);
-    gPendingBuffers.push(origBuff);
-    gLiveBuffers.delete(buff);
-  }
+  // const origBuff = gLiveBuffers.get(buff);
+  // if (origBuff) {
+  //   origBuff.fill(0);
+  //   gPendingBuffers.push(origBuff);
+  //   gLiveBuffers.delete(buff);
+  // }
 }
 
 export function decodeBase64(b64: string): Uint8Array {
